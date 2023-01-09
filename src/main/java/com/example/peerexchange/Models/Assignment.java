deba233e50dcf65d6a28746bea827d08dec33b51
gitsomemilk <m.d.bood@hotmail.com>
2023-01-09 15:05:32 +0100

    repos aangemaakt

--- src/main/java/com/example/peerexchange/Models/Assignment.java
+++ src/main/java/com/example/peerexchange/Models/Assignment.java
@@ -10,6 +10,42 @@ public class Assignment {
     @Id
     @GeneratedValue
     private Long id;
+    private String title;
+    private String description;
+    private Long class_id; // foreign key to classes.id
+
+    // getters and setters
 
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getClass_id() {
+        return class_id;
+    }
+
+    public void setClass_id(Long class_id) {
+        this.class_id = class_id;
+    }
 }
